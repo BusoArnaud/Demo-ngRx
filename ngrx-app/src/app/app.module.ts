782bc9c7ba3f264a93d4c870adfa40d8344cb51a
abuso <buso.arnaud@gmail.com>
2019-10-20 17:16:45 +0200

    Step 2-3

--- ngrx-app/src/app/app.module.ts
+++ ngrx-app/src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { Reducers } from './store/reducers/index.reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { Effects } from './store/effects/index.effects';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(Reducers,
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+          strictStateSerializability: true,
+          strictActionSerializability: true,
+        },
+      }),
+    EffectsModule.forRoot(Effects),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:
+    [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
